77096ebb5dd705a0c26c04f81d256c2320dbbe12
szidzse <gellertsos@gmail.com>
2024-03-05 14:57:56 +0100

    create POST endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,35 +2,46 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import ArtModel from "./models/ArtModel.js";
-import {fileURLToPath} from "node:url";
+import { fileURLToPath } from "node:url";
 import { join } from "node:path";
 
 const PORT = 3000;
 
-dotenv.config({path: join(fileURLToPath(import.meta.url), "/../.env")});
-const MongoURL = process.env.MONGO_URL
+dotenv.config({ path: join(fileURLToPath(import.meta.url), "/../.env") });
+const MongoURL = process.env.MONGO_URL;
 const app = express();
 
 // middleware
 app.use(express.json());
 
 app.get("/api/arts", async (req, res) => {
-  try {
-    const arts = await ArtModel.find({});
-    res.json(arts)
-  } catch (error) {
-    res.status(500).json({error: error.message})
-  }
-})
+	try {
+		const arts = await ArtModel.find({});
+		res.json(arts);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
 
+// POST - ADD NEW ART
+app.post("/api/arts", async (req, res) => {
+	const art = req.body;
+
+	try {
+		const createdArt = await ArtModel.create(art);
+		return res.json(createdArt);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
 
 async function main() {
-  await mongoose.connect(MongoURL)
+	await mongoose.connect(MongoURL);
 	console.log("Connected to database.");
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}.`);
-  });
+	app.listen(PORT, () => {
+		console.log(`Server is running on http://localhost:${PORT}.`);
+	});
 }
 
-main()
+main();
