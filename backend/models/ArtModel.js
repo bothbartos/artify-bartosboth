6e397808c9e8902666d45f4aead653c55cfab60f
Samu <108835512+nionys@users.noreply.github.com>
2024-03-05 13:44:37 +0100

    fix connection error, fix .env error

--- backend/models/ArtModel.js
+++ backend/models/ArtModel.js
@@ -10,10 +10,9 @@ const ArtSchema = new Schema({
   short_description: String,
   medium_display: String,
   artwork_type: String,
+  artwork_type_title: String,
   artist_title: String,
   image_id: String
-}, {
-  timestamps: true
 });
 
 export default model("Art", ArtSchema);
--- backend/populate/populate.js
+++ backend/populate/populate.js
@@ -2,34 +2,27 @@ import mongoose from "mongoose";
 import ArtModel from "../models/ArtModel.js";
 import dotenv from "dotenv";
 import {fileURLToPath} from "node:url";
-import { dirname, join } from "node:path";
+import { join } from "node:path";
 
-
-dotenv.config({path: join(dirname(fileURLToPath(import.meta.url)), "..", ".env")})
-
-
+dotenv.config({path: join(fileURLToPath(import.meta.url), "/../../.env")});
 const MongoURL = process.env.MONGO_URL;
-
 
 async function fetchData(){
   const response = await fetch("https://api.artic.edu/api/v1/artworks?fields=title,description,short_description,image_id,artist_title,artwork_type_title,medium_display,date_start,date_end");
   const data = await response.json();
-  return data
+  return data;
 }
-
 
 async function populateArtModel() {
   const data = await fetchData();
-  data.data.forEach(async (data) =>{
-    await ArtModel.save(data)
-  })
+  for (const art of data.data) {
+    await ArtModel.create(art);
+  }
 }
 
 async function main() {
-  mongoose.connect(MongoURL);
-
+  await mongoose.connect(MongoURL);
   await populateArtModel();
-
   await mongoose.disconnect();
 }
 
