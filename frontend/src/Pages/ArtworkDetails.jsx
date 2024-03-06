173602e2c0c58007aad6c2fa7ec28d12ef67d991
bothbartos <147398004+bothbartos@users.noreply.github.com>
2024-03-06 11:21:57 +0100

    implemented filter by artits

--- frontend/src/Pages/ArtworkDetails.jsx
+++ frontend/src/Pages/ArtworkDetails.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 async function fetchArtwork(id) {
   try {
@@ -21,6 +21,8 @@ export default function ArtworkDetails(props){
   const [loading, setLoading] = useState(true);
   
   const { id } = useParams();
+  const navigate = useNavigate();
+
   useEffect(()=>{
     fetchArtwork(id)
     .then((artwork) => {
@@ -28,6 +30,10 @@ export default function ArtworkDetails(props){
       setLoading(false)
     })
   });
+
+  function handleClick(name){
+    navigate(`/artist/${name}`)
+  }
 
   if(loading){
     return (
@@ -41,7 +47,7 @@ export default function ArtworkDetails(props){
     <div className="artworkDetails">
       <img src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}></img>
       <h3>Title: {artwork.title}</h3>
-      <h3>Artist name: {artwork.artist_title}</h3>
+      <h3 onClick={()=> handleClick(artwork.artist_title)}>Artist name: {artwork.artist_title}</h3>
       <p>{artwork.date_start === artwork.date_end ? artwork.date_start : `${artwork.date_start} - ${artwork.date_end}`}</p>
       <p>{artwork.description ? deleteHTMLTags(artwork) : "No description."}</p>
       <p>Artwork medium: {artwork.medium_display}</p>
